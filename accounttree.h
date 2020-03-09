2231bdeb6c236aafae136e1d18b35bc77dab2b82
Gibsonchan98 <59757387+Gibsonchan98@users.noreply.github.com>
2020-03-09 16:31:30 -0700

    Update accounttree.h

--- accounttree.h
+++ accounttree.h
@@ -1,5 +1,5 @@
 //
-// Created by <Name> on <Date>.
+// Created by Melanie Alexandra Palomino & Alana Yao on March 6, 2020.
 //
 
 #pragma once
@@ -18,9 +18,11 @@ public:
     // Insert new account
     bool insert(Account* account);
 
+
     // Retrieve account
     // returns true if successful AND *Account points to account
-    bool retrieve(const int& accountNumber, Account*& account) const;
+    //NOLINTNEXTLINE
+    bool retrieve(const int& accountNumber, Account*& account) ;
 
     // Display information on all accounts
     void display() const;
@@ -31,39 +33,33 @@ public:
     // check if tree is empty
     bool isEmpty() const;
 
-
 private:
     class Node {
     public:
         explicit Node(Account* account)
                 : account{account}, right{nullptr}, left{nullptr} {}
 
-        //returns account
-        auto* getAccount() const{
-            return this->account;
-        }
-        //sets account
-        void setAccount(Account*& account){
-            this->account = account;
-        }
-        //returns left pointer
-        auto* getLeft() const{
-            return this->left;
-        }
-        //returns right pointer
-        auto* getRight() const{
-            return this->right;
-        }
+        // returns account
+        auto* getAccount() const { return this->account; }
 
-        //sets node value to right pointer
-        void setRight(Node* node) {
-            this->right = node;
-        }
 
-        //sets node value to left pointer
-        void setLeft(Node* node) {
-            this->left = node;
-        }
+        // sets account
+
+        //NOLINTNEXTLINE
+        void setAccount(Account*& account) { this->account = account; }
+
+
+        // returns left pointer
+        auto* getLeft() const { return this->left; }
+        // returns right pointer
+        auto* getRight() const { return this->right; }
+
+
+        // sets node value to right pointer
+        void setRight(Node* node) { this->right = node; }
+
+        // sets node value to left pointer
+        void setLeft(Node* node) { this->left = node; }
 
     private:
         Account* account;
@@ -71,16 +67,18 @@ private:
         Node* left;
     };
 
-    Node* root;
-    //recursive insert helper function
+    Node* root = nullptr;
+
+    // recursive insert helper function
     bool insertRecursive(Node* curr, Account* account);
 
-    //recursive clear function helper
+    // recursive display function helper
+    void displayR(Node* curr) const;
+
+    // recursive clear function helper
     void clearR(Node* curr);
 
-    //recursive retrieve helper
-    bool retrieveRecursive(Node* curr,const int& acctID, Account*& account) const;
-
-    //recursive display function helper
-    void displayR(Node* curr) const;
+    // recursive retrieve helper
+    //NOLINTNEXTLINE
+    bool retrieveRecursive(Node* curr, const int& acctID, Account*& account);
 };
